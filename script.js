3108e91f4af9fb652b5673ac1371981d888c0cf0
Qakeoi <imchaxe@gmail.com>
2023-08-17 20:32:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
-document.addEventListener('DOMContentLoaded', function() {
-  // Set the date we're counting down to
-  var countDownDate = new Date("Jan 1, 2022 00:00:00").getTime();
-
-  // Update the countdown every 1 second
-  var x = setInterval(function() {
-    // Get current date and time
-    var now = new Date().getTime();
-
-    // Find the distance between now and the countdown date
-    var distance = countDownDate - now;
-
-    // Calculate days, hours, minutes, and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    // Display the countdown
-    document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-
-    // If the countdown is finished, display a message
-    if (distance < 0) {
-      clearInterval(x);
-      document.getElementById("countdown").innerHTML = "Website is live!";
+<script>
+  document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form from submitting
+    if (validateForm()) {
+      this.submit(); // If form is valid, submit the form
     }
-  }, 1000);
-});
+  });
+
+  function validateForm() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    var isValid = true;
+
+    if (name === "") {
+      isValid = false;
+      alert("Name field is required");
+    }
+
+    if (email === "") {
+      isValid = false;
+      alert("Email field is required");
+    } else if (!isValidEmail(email)) {
+      isValid = false;
+      alert("Please enter a valid email address");
+    }
+
+    if (message === "") {
+      isValid = false;
+      alert("Message field is required");
+    }
+
+    return isValid;
+  }
+
+  function isValidEmail(email) {
+    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+  }
+</script>
